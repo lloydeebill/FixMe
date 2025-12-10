12ba0ddb1c66eca074f3bcc0bf39124c877e567d
lloydeebill <lariosa.lloydbill26@gmail.com>
2025-12-10 14:09:53 +0800

    Add brevo mailer definition to config

--- config/mail.php
+++ config/mail.php
@@ -48,6 +48,9 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
+        'brevo' => [
+            'transport' => 'brevo',
+        ],
 
         'ses' => [
             'transport' => 'ses',
